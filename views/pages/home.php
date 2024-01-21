f7083b2e8014ab21f91e7ef257d3737605cab7de
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2024-01-21 16:19:17 +1000

    Added styles

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,3 +1,12 @@
-<?php
-    echo "Home"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/assets/css/app.css">
+</head>
+<body>
+    <h1>Home page</h1>
+</body>
+</html>
