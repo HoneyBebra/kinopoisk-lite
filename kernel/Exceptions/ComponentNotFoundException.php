0f202fe66bdf95c4fe586bdcf4a79c82a99001cd
HoneyBebra <128954566+HoneyBebra@users.noreply.github.com>
2024-01-22 23:29:53 +1000

    Added custom exceptions

---
+++ kernel/Exceptions/ComponentNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+    namespace App\Kernel\Exceptions;
+
+    use Exception;
+
+    class ComponentNotFoundException extends Exception {
+
+    }
+?>
--- kernel/View/View.php
+++ kernel/View/View.php
@@ -3,14 +3,26 @@
 
     class View {
         public function page(string $name): void {
+            $viewPath = APP_PATH."/views/pages/{$name}.php";
+            
+            if (!file_exists($viewPath)) {
+                throw new \App\Kernel\Exceptions\ViewNotFoundException("View {$name} not found");
+            }
+
             extract([
                 "view" => $this  // passing the var view in component 
             ]);
             
-            include(APP_PATH."/views/pages/{$name}.php");
+            include($viewPath);
         }
 
         public function component(string $name): void {
+            $componentPath = APP_PATH."/views/components/{$name}.php";
+
+            if (!file_exists($componentPath)) {
+                throw new \App\Kernel\Exceptions\ComponentNotFoundException("Component {$name} not found");
+            }
+
             include(APP_PATH."/views/components/{$name}.php");
         }
     }
